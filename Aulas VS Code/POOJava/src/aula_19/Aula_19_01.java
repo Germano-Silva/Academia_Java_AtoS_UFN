ca98696cf582e79ec87f643fb4daba4aa2929c0c
Germano-Silva <germano.silva@ufn.edu.br>
2022-11-09 08:09:06 -0300

    Create Aula_19_01.java

---
+++ Aulas VS Code/POOJava/src/aula_19/Aula_19_01.java
@@ -0,0 +1,9 @@
+package aula_19;
+
+public class Aula_19_01 {
+    private static final String HELLO_WORLD = "Hello, World!";
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(HELLO_WORLD);
+    }
+}
