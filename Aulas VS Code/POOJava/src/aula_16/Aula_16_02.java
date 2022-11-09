76dd292c83f52fd7607c364ed4c2dd8e5ccc2490
Germano-Silva <germano.silva@ufn.edu.br>
2022-11-09 08:07:48 -0300

    Create Aula_16_02.java

---
+++ Aulas VS Code/POOJava/src/aula_16/Aula_16_02.java
@@ -0,0 +1,18 @@
+package aula_16;
+
+public class Aula_16_02 {
+    public static void main(String[] args)throws Exception{
+        if(args.length > 0){
+            int[] intArgs = new int[args.length];
+            for(int i = 0; i < intArgs.length; i++){
+                try{
+                    intArgs[i] = Integer.parseInt(args[i]);
+                    System.out.format("Argumento inteiro %d = %d%n", i, intArgs[i]);
+                }catch(NumberFormatException e){
+                    System.err.println("Argumento " +args[i]+ " deve ser um inteiro.");
+                    System.exit(1);
+                }
+            }
+        }
+    }
+}
