f93cc233578967b44d45d1e52f9b1709fc51072b
Germano-Silva <germano.silva@ufn.edu.br>
2022-11-09 08:07:11 -0300

    Create Aula_16_01.java

---
+++ Aulas VS Code/POOJava/src/aula_16/Aula_16_01.java
@@ -0,0 +1,9 @@
+package aula_16;
+
+public class Aula_16_01 {
+    public static void main(String[] args) throws Exception{
+        for(String s : args){
+            System.out.println(s);
+        }
+    }
+}
